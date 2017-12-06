72dbc64c6f19f142df00e6f55535ecdfba643402
Miloš Mošić <mosic.milos@gmail.com>
2017-12-06 20:39:30 +0100

    Build binaries only for the current platform by default

--- scripts/build_binaries.sh
+++ scripts/build_binaries.sh
@@ -2,6 +2,17 @@
 
 set -x
 
+if [ "$(uname)" == "Darwin" ];
+then
+    OS=mac
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];
+then
+    OS=linux
+else
+  echo "Platform not supported"
+  exit 1
+fi
+
 COMMIT_HASH=5c8bb8adb87d2977976efc05c9a44bfccafc3188
 ROOT_DIR=$(pwd)
 BIN_DIR=$ROOT_DIR/bin
@@ -23,14 +34,23 @@ cd $TMP_DIR/go-ethereum
 git fetch --depth 1 origin $COMMIT_HASH
 git checkout $COMMIT_HASH
 
-./build/env.sh go run build/ci.go xgo -- --go=latest --targets=$TARGETS -v ./cmd/geth
-./build/env.sh go run build/ci.go xgo -- --go=latest --targets=$TARGETS -v ./cmd/swarm
+if [[ $1 == "lmw" ]]
+then
+    ./build/env.sh go run build/ci.go xgo -- --go=latest --targets=$TARGETS -v ./cmd/geth
+    ./build/env.sh go run build/ci.go xgo -- --go=latest --targets=$TARGETS -v ./cmd/swarm
 
-cp build/bin/geth-linux-* $BIN_DIR/geth-linux
-cp build/bin/geth-darwin-* $BIN_DIR/geth-mac
-cp build/bin/geth-windows-* $BIN_DIR/geth-win
-cp build/bin/swarm-linux-* $BIN_DIR/swarm-linux
-cp build/bin/swarm-darwin-* $BIN_DIR/swarm-mac
-cp build/bin/swarm-windows-* $BIN_DIR/swarm-win
+    cp build/bin/geth-linux-* $BIN_DIR/geth-linux
+    cp build/bin/geth-darwin-* $BIN_DIR/geth-mac
+    cp build/bin/geth-windows-* $BIN_DIR/geth-win
+    cp build/bin/swarm-linux-* $BIN_DIR/swarm-linux
+    cp build/bin/swarm-darwin-* $BIN_DIR/swarm-mac
+    cp build/bin/swarm-windows-* $BIN_DIR/swarm-win
+else
+  ./build/env.sh go run build/ci.go install ./cmd/geth
+  ./build/env.sh go run build/ci.go install ./cmd/swarm
+
+  cp build/bin/geth $BIN_DIR/geth-$OS
+  cp build/bin/swarm $BIN_DIR/swarm-$OS
+fi
 
 set +x
