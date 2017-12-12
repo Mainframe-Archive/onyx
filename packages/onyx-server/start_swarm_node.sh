776ae846edb1168e9d52abe84e1dc0401ea3ccdc
Shane Howley <shane@mainframe.com>
2017-12-12 14:00:11 +0000

    Fix bootnode IP

--- packages/onyx-server/start_swarm_node.sh
+++ packages/onyx-server/start_swarm_node.sh
@@ -49,7 +49,7 @@ $GODIR/build/bin/swarm \
     --ens-api '' \
     --pss \
     --bzznetworkid 922 \
-    --bootnodes enode://e834e83b4ed693b98d1a31d47b54f75043734c6c77d81137830e657e8b005a8f13b4833efddbd534f2c06636574d1305773648f1f39dd16c5145d18402c6bca3@54.171.164.15:30399 \
+    --bootnodes enode://e834e83b4ed693b98d1a31d47b54f75043734c6c77d81137830e657e8b005a8f13b4833efddbd534f2c06636574d1305773648f1f39dd16c5145d18402c6bca3@52.51.239.180:30399 \
     --ws \
     --wsorigins '*'
 
